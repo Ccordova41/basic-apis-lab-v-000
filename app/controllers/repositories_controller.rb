7f4929f0eb2c81c8c96a9e3b2e37206b48ca44c5
Catherine Cordova <ccordova41@gmail.com>
2017-11-29 01:23:35 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,3 +7,13 @@ class RepositoriesController < ApplicationController
   def github_search
   end
 end
+
+
+Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = '086201935cabc1661dcd'
+      req.params['client_secret'] = 'd2238baaa2e93c2eb5c87d1e1d36af1e135dddb3'
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
+    end
+    render 'search'
